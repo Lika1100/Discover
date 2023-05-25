e8808b4a5572d19896cce8aa0be0b9a4b640edf5
Lika <akoesnikova@gmail.com>
2023-05-25 21:23:12 +0300

    add new section service plus slider slick plus adaptive

--- js/script.js
+++ js/script.js
@@ -7,4 +7,17 @@ $(function () {
         }
 
     })
+
+    $('.service-slider').slick({
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                centerMode: true,
+                centerPadding: '50px',
+            }
+        }]
+    })
 })
